96514fe2ed2a6ed8739c20f02fc05a4a0a28bf53
Alexander Papaspyrou <alexander@papaspyrou.name>
2011-11-11 12:07:48 +0100

    Revert "First attempt to Python 2.7 bump."
    This reverts commit d494aed2417fdba513a06f4013f261bc831cc95d.

--- dyso/app.py
+++ dyso/app.py
@@ -16,21 +16,22 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with doyouspeakOCCI.  If not, see <http://www.gnu.org/licenses/>.
-
 import logging
+import uuid
 
 from dyso.main import MainPage
+from dyso.model import Detail, Test, Suite
+
 from dyso.statistics import StatisticsPage
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 
-app = webapp.WSGIApplication(
-    [
-        ('/', MainPage),
-        ('/statistics', StatisticsPage),
-    ], debug=True
+application = webapp.WSGIApplication([
+    ('/', MainPage),
+    ('/statistics', StatisticsPage),
+], debug=True
 )
 
 
@@ -39,7 +40,7 @@ def main():
     TODO: not yet commented.
     """
     logging.debug('Starting dyso WSGI application...')
-    run_wsgi_app(app)
+    run_wsgi_app(application)
     logging.debug('dyso WSGI application is up and running!')
 
 
